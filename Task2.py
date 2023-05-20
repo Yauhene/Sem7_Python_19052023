6d6ea85319ec0bb87491049d18acc2d500350f5b
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-05-20 07:22:56 +0300

    Done

---
+++ Task2.py
@@ -0,0 +1,16 @@
+﻿# 2) Дан список, вывести отдельно буквы и цифры, пользуясь filter.
+
+# [12,'sadf',5643] ---> ['sadf'] ,[12, 5643]
+
+
+# 3) Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр
+
+# 123  --> 6
+# 1.23 --> 6
+
+list_2 = [12, 'sadf', 5643]
+
+result_list_2 = list(filter(lambda x: type(x) == int, list_2))
+result_list_3 = list(filter(lambda x: type(x) != int, list_2))
+
+print(result_list_2, result_list_3)
